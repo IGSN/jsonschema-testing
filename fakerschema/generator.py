0215e78b4aecb3f5ca10373fa5ccf3624decb16b
Jess Robertson <jesse.robertson@outlook.com.au>
2020-02-14 06:29:40 +0800

    🦴 Adding basic generator skeleton

---
+++ fakerschema/generator.py
@@ -0,0 +1,30 @@
+from random import randint
+
+from faker import Faker
+
+from . import fakers
+
+def should_include(key, required_list):
+    """
+    Probabilistically include non-required keys in a document.
+
+    Required keys are always included. Optional keys are included
+    based on a coin flip (i.e. a uniform distribution over {0, 1}).
+
+    Parameters:
+        key - the key to check
+        required_list - the required properties from the schema
+    """
+    if key in required_list:
+        return True
+    else: 
+        return bool(randint(0, 1))
+
+def generate(schema):
+    """
+    Generate a bunch of data given a JSONSchema instance.
+
+    Parameters:
+        schema - the JSONSchema object to use.
+    """
+    # fake = Faker(?)
---
+++ fakerschema/main.py
@@ -0,0 +1,22 @@
+import click
+import jsonref as json
+
+from .generator import generate
+
+
+@click.command()
+@click.option("-n", "--number", default=1, help="Number of data points to generate")
+@click.option("-l", "--lines", is_flag=True, help="Output in JSONLines format")
+@click.argument("schema", type=click.File(mode="r", encoding="utf-8", errors="strict"))
+def main(schema, number, lines):
+    "Main script entry."
+    output = {
+        'schema': json.load(schema),
+        'format': "JSONlines" if lines else "JSON",
+        'n_objects': number
+    }
+
+    click.echo(json.dumps(output))
+
+if __name__ == "__main__":
+    main()  # pylint: disable=E1120
